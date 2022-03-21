e9aedf06a424213b993b0a04be6fc331c8a6e5e9
RahulSiddhardh <49991050+RahulSiddhardh@users.noreply.github.com>
2022-03-21 19:51:20 +0530

    Update TestP1.py

--- TestP1.py
+++ TestP1.py
@@ -6,10 +6,9 @@ from Program1 import summation
 
 class TestSum(unittest.TestCase):
       def test_list_int(self):
-
-         data = [24, 6]
-         result = summation(data)
-         self.assertEqual(result, 30)
+            data = [24, 6]
+            result = summation(data)
+            self.assertEqual(result, 30)
       
 if __name__ == "__main__":
       unittest.main()
