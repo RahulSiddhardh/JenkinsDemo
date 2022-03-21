77810594cbdebd1e11f0cbfaca7d03a14162d54f
RahulSiddhardh <49991050+RahulSiddhardh@users.noreply.github.com>
2022-03-21 19:53:41 +0530

    Update TestP1.py

--- TestP1.py
+++ TestP1.py
@@ -8,7 +8,7 @@ class TestSum(unittest.TestCase):
       def test_list_int(self):
             data = [2, 6]
             result = summation(data)
-            self.assertEqual(result, 30)
+            self.assertEqual(result, 8)
       
 if __name__ == "__main__":
       unittest.main()
